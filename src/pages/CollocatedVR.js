9c3ca5d0e294fab309f98cc2a1e58a3a49981b90
Kontrazis Petros <kontrazis-petros@hotmail.com>
2021-10-08 12:31:11 +0300

    Collocated VR ready to start

---
+++ src/pages/CollocatedVR.js
@@ -0,0 +1,18 @@
+import { Layout } from '../layout/Layout';
+import { Section, SectionDivider, SectionTitle } from '../styles/GlobalComponents';
+import { GridContainer, CenterImage } from '../components/Projects/ProjectsStyles';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { xonokai } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+
+const CollocatedVR =() =>{
+	return(
+	<Layout>
+		<div>
+
+		</div>
+		<br/>
+	</Layout>
+	)
+}
+
+export default CollocatedVR
