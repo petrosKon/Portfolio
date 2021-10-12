a9e9f687d3c3085c18a169611e1a4b9de5981763
Kontrazis Petros <kontrazis-petros@hotmail.com>
2021-10-12 12:49:54 +0300

    Timeline finished

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -78,9 +78,9 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2017, text: 'Diploma thesis in Unity', },
+  { year: 2018, text: 'Worked as an Android developer in Ingredio', },
+  { year: 2019, text: 'Worked as a Unity/Android developer in COMIC', },
+  { year: 2020, text: 'Worked as a Unity developer at COMIC & MAGOS', },
+  { year: 2021, text: 'Master & Internship in IP Paris', },
 ];
