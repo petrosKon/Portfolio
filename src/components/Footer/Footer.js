3641348d801db6d9b44a36d3128497a7b1b28c9c
Kontrazis Petros <kontrazis-petros@hotmail.com>
2021-10-14 08:50:10 +0300

    Slogan and social on Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,6 +17,17 @@ const Footer = () => {
                     <LinkItem href="mailto:kontrazis-petros@hotmail.com">kontrazis-petros@hotmail.com</LinkItem>
             </LinkColumn>
         </LinkList>
+        <SocialIconsContainer>
+        <CompanyContainer>
+            <Slogan>Passion makes every project perfect</Slogan>
+        </CompanyContainer>
+         <SocialIcons href="https://github.com/petrosKon">
+            <AiFillGithub size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href="https://www.linkedin.com/in/petros-k-248739168/">
+            <AiFillLinkedin size="3rem"/>
+        </SocialIcons>
+        </SocialIconsContainer>
     </FooterWrapper>
   );
 };
